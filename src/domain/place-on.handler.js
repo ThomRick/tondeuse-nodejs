b5c4b7a6e04b547248b17cd5b973099b82df2bb9
thomrick <tricart@xebia.fr>
2018-03-09 20:10:18 +0100

    #event-sourcing : develop place on handler

---
+++ src/domain/place-on.handler.js
@@ -0,0 +1,16 @@
+class PlaceOnHandler {
+  constructor(mowerRepository, fieldRepository) {
+    this.mowerRepository = mowerRepository;
+    this.fieldRepository = fieldRepository;
+  }
+
+  placeOn(mowerId, fieldId) {
+    const mower = this.mowerRepository.get(mowerId);
+    const field = this.fieldRepository.get(fieldId);
+    mower.placeOn(field);
+    this.mowerRepository.save(mower);
+    return mower;
+  }
+}
+
+module.exports = { PlaceOnHandler };
---
+++ test/domain/place-on.handler.test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+chai.should();
+
+const Mower = require('../../src/domain/aggregate/mower');
+const Orientation = require('../../src/domain/aggregate/orientation');
+const Position = require('../../src/domain/aggregate/position');
+
+const Field = require('../../src/domain/aggregate/field');
+const Dimension = require('../../src/domain/aggregate/dimension');
+
+const { InMemoryFieldRepository } = require('../../src/infra/database/in-memory-field.repository');
+const { InMemoryMowerRepository } = require('../../src/infra/database/in-memory-mower.repository');
+const { PlaceOnHandler } = require('../../src/domain/place-on.handler');
+
+describe('Place On Handler', () => {
+  let mowerRepository;
+  let fieldRepository;
+  let placeOnHandler;
+  before(() => {
+    mowerRepository = new InMemoryMowerRepository();
+    fieldRepository = new InMemoryFieldRepository();
+    placeOnHandler = new PlaceOnHandler(mowerRepository, fieldRepository);
+  });
+  it('should link a mower and a field by placing the mower on it', () => {
+    const mower = Mower.Builder().withPosition(Position.at(0, 0)).withOrientation(Orientation.from(Orientation.NORTH)).build();
+    mowerRepository.save(mower);
+    const field = Field.Builder().withDimension(Dimension.of(4, 4)).build();
+    fieldRepository.save(field);
+    const placedOnMower = placeOnHandler.placeOn(mower.getId(), field.getId());
+    placedOnMower.getPosition().should.be.deep.equal(mower.getPosition());
+    placedOnMower.getOrientation().should.be.deep.equal(mower.getOrientation());
+    placedOnMower.getField().getDimension().should.be.deep.equal(field.getDimension());
+  });
+});
