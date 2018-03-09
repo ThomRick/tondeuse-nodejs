41d7f2c411e5d1ed6d7d5a152a7e8df384b51322
thomrick <tricart@xebia.fr>
2018-03-09 14:48:31 +0100

    #database : create in memory database

---
+++ src/infra/database/repositories/in-memory-field.repository.js
@@ -0,0 +1,18 @@
+const Field = require('../../../domain/field');
+
+class InMemoryFieldRepository {
+  constructor(database = new Map()) {
+    this.database = database;
+  }
+
+  save(field) {
+    const events = this.database.get(field.getId()) || [];
+    this.database.set(field.getId(), events.concat(field.getUncommittedChanges()));
+  }
+
+  get(fieldId) {
+    return Field.rebuild(this.database.get(fieldId));
+  }
+}
+
+module.exports = { InMemoryFieldRepository };
