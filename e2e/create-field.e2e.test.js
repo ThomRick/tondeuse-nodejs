9d4bce27842d46ca3afc36b617df41977ab833a3
thomrick <tricart@xebia.fr>
2018-03-13 11:52:47 +0100

    #execute-program : develop create field e2e test

--- e2e/create-field.e2e.test.js
+++ e2e/create-field.e2e.test.js
@@ -1,8 +1,29 @@
 const chai = require('chai');
 chai.should();
 
+const request = require('request');
+
 describe('Create Field', () => {
-  it('can be run', () => {
-    console.log('Hello E2E Test');
+  it('should return the created field', () => {
+    const fieldToCreate = {
+      dimension: {
+        width: 4,
+        length: 4
+      }
+    };
+    return request.post({
+      url: 'http://localhost:3000/api/fields',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(fieldToCreate)
+    }, (error, response, body) => {
+      response.status.should.be.equal(201);
+      JSON.parse(body).should.have.property('id');
+      JSON.parse(body).should.have.property('dimension', {
+        width: 4,
+        length: 4
+      });
+    });
   });
 });
